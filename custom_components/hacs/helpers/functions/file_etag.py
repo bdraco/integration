9bdb76e2a9e88ef1715dec06397552cf6785b052
J. Nick Koston <nick@koston.org>
2020-12-18 14:41:46 -1000

    Use built-in hash function for etag performance

--- custom_components/hacs/helpers/functions/file_etag.py
+++ custom_components/hacs/helpers/functions/file_etag.py
@@ -2,7 +2,6 @@
 import os
 
 from custom_components.hacs.share import get_hacs
-from fnvhash import fnv1a_32
 from pathlib import Path
 
 
@@ -10,12 +9,12 @@ def get_etag(path) -> bool:
     file_path = Path(path)
     if not file_path.exists():
         return None
-    return fnv1a_32(file_path.read_bytes())
+    return hash(file_path.read_bytes())
 
 
 async def async_get_etag(path) -> bool:
     hass = get_hacs().hass
-    fnv = await hass.async_add_executor_job(get_etag, path)
-    if fnv is None:
+    etag = await hass.async_add_executor_job(get_etag, path)
+    if etag is None:
         return None
-    return str(hex(fnv))
+    return '"' + str(hex(etag)) + '"'
